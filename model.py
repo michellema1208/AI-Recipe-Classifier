18362273c3bbd7e3c06ed4e3d84015577332731b
Michelle Ma <mma1@swarthmore.edu>
2018-05-15 22:23:15 -0400

    finished

--- model.py
+++ model.py
@@ -20,16 +20,12 @@ for i in range(len(trainIngredients)):
 
 vectorizer = CountVectorizer(input = "content")
 boolTrainCuisine = vectorizer.fit_transform(trainCuisine).toarray()
-print("length of boolTrainCuisine", boolTrainCuisine.shape)
 boolTrainCuisine = boolTrainCuisine.argmax(1)
 
 #### TEST ####
 test = np.load("test.npz")
 boolTestIngredients = test["boolTestIngredients"]
 boolTestCuisine = test["boolTestCuisine"]
-print("length of test ingredients", len(boolTestIngredients))
-print("length of train ingredients", len(boolTrainIngredients))
-print("length of all ingredients", len(allIngredients))
 
 #### NEURALNET ####
 neural_net = Sequential()
@@ -54,12 +50,9 @@ print("NN accuracy: {}%".format(accuracy*100))
 
 
 #### RANDOMFOREST ####
-forest = RandomForestClassifier(n_estimators=500, max_features='auto', class_weight='balanced')
+forest = RandomForestClassifier(n_estimators=100, max_features='auto', class_weight='balanced')
 forest.fit(boolTrainIngredients, boolTrainCuisine)
 yTest = forest.predict(boolTestIngredients)
-#print("random forest yTest", yTest)
-#predictLabels = np.argmax(yTest, axis = 1)
-#print("predictLabels", predictLabels)
 print("#### RANDOMFOREST ####")
 for i in range(len(yTest)):
     for j in range(len(boolTrainCuisine)):
@@ -68,41 +61,3 @@ for i in range(len(yTest)):
     print(i, "predicted label", cuisineLabel)
 accuracy = forest.score(boolTestIngredients, boolTestCuisine)
 print("RF accuracy: {}%".format(accuracy*100))
-
-
-"""#TODO: build a model"
-neural_net = Sequential()
-neural_net.add(Conv2D(5, (2, 2), activation='relu'))
-neural_net.add(Dropout(0.1))
-neural_net.add(Conv2D(5, (2, 2), activation='relu'))
-neural_net.add(Flatten())
-neural_net.add(Dense(64, activation='softmax'))
-
-neural_net.summary()
-#TODO: train the model"
-
-neural_net.compile(optimizer="Adamax", loss="categorical_crossentropy", metrics=['accuracy'])
-history = neural_net.fit(train_X, train_Y, verbose=1, validation_data=(test_X, test_Y), epochs=10)
-
-loss, accuracy = neural_net.evaluate(test_X, test_Y, verbose=0)
-print("accuracy: {}%".format(accuracy*100))
-
-#TODO: test the model
-
-for i in range(10):
-    index = np.random.randint(test_X.shape[0])
-    shiftIndex = index+(4*states.shape[0] // 5)
-
-    #correct answer
-    print(show_move(test_X[index], moves[shiftIndex], turns[shiftIndex]))
-    print(show_values(test_X[index], values[shiftIndex]))
-    print("shiftIndex", moves[shiftIndex])
-    predictedMove = neural_net.predict(test_X)[index]
-    moveIndex = np.argmax(predictedMove)
-    movePair = (moveIndex//8, moveIndex-(moveIndex//8)*8)
-    print("movepair", movePair)
-    #prediction
-    print(show_move(test_X[index], movePair, turns[shiftIndex]))
-    print(show_values(test_X[index], values[shiftIndex]))
-
-"""
